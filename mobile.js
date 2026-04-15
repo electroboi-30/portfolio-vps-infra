d47e1cb221677044cdee6d7f885232c0108fc0cb
Pravin K <pravin.n@icloud.com>
2026-04-15 16:52:05 +0530

    fix

--- mobile.js
+++ mobile.js
@@ -24,40 +24,39 @@ async function typeText(text) {
 
 async function run(cmd) {
 
-  // cancel previous animation instantly
-  currentTypingId++;
+  currentTypingId++; // cancel previous animation
 
   if (cmd === "stats") {
     await typeText("> Fetching live stats...\n");
 
     try {
-    const res = await fetch("../api/stats");
-    const data = await res.json();
+      const res = await fetch("/api/stats");
+      const data = await res.json();
 
-    // Convert uptime from mins → hrs + mins
-    const mins = parseInt(data.uptime);
-    const days= Math.floor(mins/1440)
-    const remainingAfterDays = mins % 1440;
-    const hours = Math.floor(remainingAfterDays / 60);
-    const remainingMins = remainingAfterDays % 60;
+      const mins = parseInt(data.uptime);
+      const days = Math.floor(mins / 1440);
+      const remainingAfterDays = mins % 1440;
+      const hours = Math.floor(remainingAfterDays / 60);
+      const remainingMins = remainingAfterDays % 60;
 
-    let uptimeText = "";
-    if(days > 0){
-      uptimeText = `${days} days ${hours} hrs ${remainingMins} mins`;
-    }
-    else if (hours > 0) {
-      uptimeText = `${hours} hrs ${remainingMins} mins`;
-    } else {
-      uptimeText = `${remainingMins} mins`;
-    }
+      let uptimeText = "";
+      if (days > 0) {
+        uptimeText = `${days} days ${hours} hrs ${remainingMins} mins`;
+      } else if (hours > 0) {
+        uptimeText = `${hours} hrs ${remainingMins} mins`;
+      } else {
+        uptimeText = `${remainingMins} mins`;
+      }
 
-    return `
-    CPU Usage: ${data.cpu}
-    RAM Usage: ${data.ram}
-    Uptime: ${uptimeText}`;
+      await typeText(`
+      CPU Usage: ${data.cpu}
+      RAM Usage: ${data.ram}
+      Uptime: ${uptimeText}`);
+
     } catch {
       await typeText("Error fetching stats");
     }
+
     return;
   }
 
